4d7580a8e7d5b199426dc819a5ed2c063969d722
Damian <damianmartinezrx@gmail.com>
2025-09-15 15:18:09 -0300

    actualizar calculadora

---
+++ main.py
@@ -0,0 +1,47 @@
+def suma (a, b):
+    return a + b
+def resta (a, b):
+    return a - b
+def multiplicacion (a, b):
+    return a * b
+def division (a, b):
+    return a / b
+
+def menu():
+    print("Bienvenido a la calculadora  simple  en Python\n")
+    print("1.Suma")
+    print("2.Resta")
+    print("3.Multiplicacion")
+    print("4.Division")
+    print("5.Salir")    
+
+def main(): 
+    while True:
+        menu()
+        try:    
+            option = int(input("\nSeleccione una opcion (1-5): "))
+            num1 = float(input("Ingrese el primer numero: "))
+            num2 = float(input("Ingrese el segundo numero: "))          
+            match option:
+                case 1:
+                    resulta = suma(num1, num2)
+                case 2:
+                    resultado=resta(num1, num2 )             
+                case 3:
+                    resulta=multiplicacion(num1, num2  )
+                case 4:
+                    resultado=division(num1, num2  )      
+                case 5:   
+                    print("Saliendo de la calculadora. ¡Hasta luego!")
+                    break   
+                case _:
+                    print("Opcion invalida. Por favor, seleccione una opcion valida (1-5)./n")
+                    
+        except ValueError:
+            print("Error: Debe ingresar un numero valido")
+        except ZeroDivisionError:
+            print("Error: No se puede dividir por cero")
+        else:
+            print(f"El resultado es: {resultado}")
+    
+main()
